0e48c5767723a464f0a5a085c67d0ed56aa7f6c6
Kolya Korobochkin <me@korobochkin.com>
2016-10-21 22:33:16 +0300

    arhive template with sidebar on demand

--- archive.php
+++ archive.php
@@ -9,34 +9,50 @@ get_header(); ?>
 
 <div id="main" class="site-main" role="main">
 	<div id="content" class="site-content">
-		<div id="primary" class="content-area">
 
-		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-			
-			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-
-			<?php endwhile; ?>
-
-			<?php maker_posts_pagination(); ?>
-
+		<?php if( maker_mariupol_is_sidebar_enabled() ) : ?>
+			<div id="primary" class="content-area">
 		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
+			<div id="primary" class="content-area-wide">
 		<?php endif; ?>
 
-		</div><!-- #primary -->
 
-		<?php get_sidebar(); ?>
+
+			<?php if ( have_posts() ) : ?>
+
+				<header class="page-header">
+					<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
+					?>
+				</header><!-- .page-header -->
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+
+				<?php endwhile; ?>
+
+				<?php maker_posts_pagination(); ?>
+
+			<?php else : ?>
+
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+			<?php endif; ?>
+
+
+
+		<?php if( maker_mariupol_is_sidebar_enabled() ) : ?>
+			</div><!-- #primary -->
+			<?php get_sidebar(); ?>
+		<?php else : ?>
+			</div><!-- #primary -->
+		<?php endif; ?>
+
+
 
 	</div><!-- #content -->
 </div><!-- #main -->
