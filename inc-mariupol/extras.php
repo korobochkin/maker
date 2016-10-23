f196f0045b06866304c94928939720740c4201e5
Kolya Korobochkin <me@korobochkin.com>
2016-10-23 17:47:44 +0300

    no sidebar for singular posts

--- inc-mariupol/extras.php
+++ inc-mariupol/extras.php
@@ -1,7 +1,7 @@
 <?php
 function maker_mariupol_body_classes( $classes ) {
 
-	if( is_home() || is_archive() ) {
+	if( is_home() || is_archive() || is_singular() ) {
 		$classes[] = 'no-sidebar';
 	}
 
