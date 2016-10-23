32b207e5ed512165017abbb763a1f991b085fdfb
Kolya Korobochkin <me@korobochkin.com>
2016-10-23 17:47:27 +0300

    relocate templates

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ get_header(); ?>
 
 					<?php while ( have_posts() ) : the_post(); ?>
 
-						<?php get_template_part( 'template-parts/cards/content', get_post_format() ); ?>
+						<?php get_template_part( 'template-parts-mariupol/cards/content', get_post_format() ); ?>
 
 					<?php endwhile; ?>
 
