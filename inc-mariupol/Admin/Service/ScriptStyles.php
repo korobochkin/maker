6022ac91841907ad7429c0a18bc575e3c3934bc8
Kolya Korobochkin <me@korobochkin.com>
2016-11-07 20:41:29 +0300

    rename css

--- inc-mariupol/Admin/Service/ScriptStyles.php
+++ inc-mariupol/Admin/Service/ScriptStyles.php
@@ -6,7 +6,7 @@ class ScriptStyles {
 	public static function register() {
 		wp_enqueue_style(
 			'maker-mariupol-admin-styles',
-			get_template_directory_uri() . '/admin-styles.css',
+			get_template_directory_uri() . '/admin-style.css',
 			array(),
 			MAKER_VERSION
 		);
